90324226bc86a65c7b841097d338922a1bec8c2a
Emmanuel ULRICH <eulrich@hotmail.fr>
2024-09-12 13:40:45 +0000

    commit 4

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,6 +1,12 @@
 
 -- Use the `ref` function to select from other models
 
-select *
-from {{ ref('my_first_dbt_model') }}
-where id = 1
+CREATE TABLE IF NOT EXISTS employees (
+    employee_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE,
+    hire_date DATE DEFAULT CURRENT_DATE,
+    salary NUMERIC(10, 2),
+    department_id INTEGER REFERENCES departments(id)
+);
